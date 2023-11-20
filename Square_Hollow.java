fcd9b600f2884babe0b6a921df4160f2cd592bf5
Shahriar Shimul <108885312+shahriarshimul@users.noreply.github.com>
2023-11-21 01:58:43 +0600

    Add files via upload

---
+++ Square_Hollow.java
@@ -0,0 +1,46 @@
+// ******
+// *    *
+// *    *
+// *    *
+// *    *
+// ******
+
+// printing pattern- Square Hollow pattern
+
+import java.util.Scanner;
+public class SquareHollow{
+    
+    public static void printPattern(int n)
+    {
+        int i,j;
+        // Outer loop to handle number of throws
+        for (i=0;i<n;i++)
+        {
+            // inner loop to handle number of columns
+            
+        for(j=0;j<n;j++)
+        {
+            // star will print only when it is in first throws
+            // or last row or first column or last column
+            
+            if (i == 0 || j == 0 || i == n-1 || j == n-1)
+            {
+                System.out.print("*");
+            }
+            // Otherwise print space only
+            else
+            {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        }
+    }
+    // Driver Function
+    
+    public static void main(String[] args)
+    {
+        int n=6;
+        printPattern(n);
+    }
+}
